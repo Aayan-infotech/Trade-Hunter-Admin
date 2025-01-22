12b999e88ed3806ee2d39d59f8b3404a303b082b
Aayaninfotech <Aayaninfotech>
2025-01-22 15:41:36 +0530

    provider magement

--- src/views/Users/JobsManagemen.js
+++ src/views/Users/JobsManagemen.js
@@ -64,43 +64,45 @@ const JobsManagemen = () => {
             <div className="card-body">
               <h5 className="card-title text-primary mb-2"><strong>{job.title}</strong></h5>
               <div className="row">
-                <div className="col-md-6 mb-2">
+                <div className="col-md-4 mb-2">
                   <p className="mb-1"><strong>Location:</strong></p>
                   <p className="text-muted">{job.location?.jobaddress || 'N/A'}</p>
                 </div>
-                <div className="col-md-6 mb-2">
+                <div className="col-md-4 mb-2">
                   <p className="mb-1"><strong>Estimated Budget:</strong></p>
                   <p className="text-muted">${job.estimatedBudget}</p>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-md-6 mb-2">
+                <div className="col-md-4 mb-2">
                   <p className="mb-1"><strong>Service Type:</strong></p>
                   <p className="text-muted">{job.serviceType}</p>
                 </div>
-                <div className="col-md-6 mb-2">
+              </div>
+              <div className="row">
+                <div className="col-md-4 mb-2">
                   <p className="mb-1"><strong>Service:</strong></p>
                   <p className="text-muted">{job.service}</p>
+                </div>
+                <div className="col-lg-4">
+                  <p className="mb-1"><strong>Requirements:</strong></p>
+                  <p className="text-muted">{job.requirements}</p>
+                </div>
+                <div className="col-lg-4">
+                  <p className="mb-1"><strong>Timeframe:</strong></p>
+                  <p className="text-muted">
+                    {new Date(job.timeframe.from).toLocaleString()} to {new Date(job.timeframe.to).toLocaleString()}
+                  </p>
                 </div>
               </div>
 
               <div className='row gy-2'>
-                <div className="col-lg-6">
-                  <p className="mb-1"><strong>Requirements:</strong></p>
-                  <p className="text-muted">{job.requirements}</p>
-                </div>
+
                 <div className="col-lg-6">
                   <p className="mb-1"><strong>Job Status:</strong></p>
                   <span className="fs-5">{getStatusBadge(job.jobStatus)}</span>
 
                 </div>
               </div>
-              <div>
-                <p className="mb-1"><strong>Timeframe:</strong></p>
-                <p className="text-muted">
-                  {new Date(job.timeframe.from).toLocaleString()} to {new Date(job.timeframe.to).toLocaleString()}
-                </p>
-              </div>
+
 
             </div>
           </div>
